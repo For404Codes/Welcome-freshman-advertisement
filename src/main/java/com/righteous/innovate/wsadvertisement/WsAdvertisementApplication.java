e593aadab0fa24eb5f60832957a58c63a711e1ca
For404Codes <zrj2214537@163.com>
2024-01-22 22:03:46 +0800

    大致框架（未测试，未添加数据库）

---
+++ src/main/java/com/righteous/innovate/wsadvertisement/WsAdvertisementApplication.java
@@ -0,0 +1,13 @@
+package com.righteous.innovate.wsadvertisement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsAdvertisementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsAdvertisementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/righteous/innovate/wsadvertisement/WsAdvertisementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.righteous.innovate.wsadvertisement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsAdvertisementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
